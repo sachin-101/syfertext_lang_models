7e432377fa45517b0f18bb13cf07e6f34a61565d
sachin-101 <sachinkumar04428@gmail.com>
2020-03-16 15:14:28 +0530

    code refraction

--- script.py
+++ script.py
@@ -1,17 +1,14 @@
-
 import spacy
 import pickle
 import mmh3
 import os
 import numpy as np
 
-# This script does the following:
-# 1. Loads a spacy language model
-# 2. Creates a language model for SyferText, with the following contents:
-#     a. vectors (binary file) - dictionary{keys:index, values: vectors}
-#     b. key2rows (binary file) - dictionary {keys: as hashes of token texts,
+# This script creates a language model for SyferText, with the following files:
+#     a. vectors - numpy array with dimension (vocab_size, embed_dim)
+#     b. key2row - dictionary {keys: hashes of token texts,
 #                         values: index in 'vectors' of the corresponding vector} 
-#     c. words (binary file) - list of strings for the StringObject of the spacy language model
+#     c. words - list of strings from the StringObject of the spacy language model
 
 def hash_string(string):
     key = mmh3.hash64(string, signed=False, seed=1)[0]
@@ -22,19 +19,18 @@ def save_file(obj, filename):
         pickle.dump(obj, f)
 
 def create_lang_model(nlp):
-    vocab_words = list(nlp.vocab.strings)[1:]  # Avoiding the first empty char
-    vocab_size = len(vocab_words)
-    embed_dim = nlp(vocab_words[0]).vector.shape[0]
-    vectors = np.zeros((vocab_size, embed_dim))
-    key2row = {}
-    words = []
-    for index, word in enumerate(vocab_words):
-        print(index, word)
+    vocab = list(nlp.vocab.strings)     
+    vocab_size = len(vocab)              
+    embed_dim = nlp(vocab[0]).vector.shape[0]
+    vectors = np.zeros((vocab_size, embed_dim)) 
+    key2row = {}    
+    words = []      
+    for index, word in enumerate(vocab):
         vector = nlp(word).vector
         key = hash_string(word)
-        key2row[key] =  index
-        vectors[index] =  vector
-        words.append(word) 
+        key2row[key] =  index   # key and row of corresponding vector
+        vectors[index] =  vector   
+        words.append(word)
 
     save_file(vectors, filename='vectors')
     save_file(key2row, filename='key2row')
