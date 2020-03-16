d084364489cfa0edf57b97cec51cc48e49d52440
sachin-101 <sachinkumar04428@gmail.com>
2020-03-16 23:08:18 +0530

    Added en_core_web_sm language model

--- script.py
+++ script.py
@@ -18,26 +18,38 @@ def save_file(obj, filename):
     with open(filename, 'wb') as f:
         pickle.dump(obj, f)
 
-def create_lang_model(nlp):
+def create_lang_model(model):
+    nlp = spacy.load(model)     # Load the spacy nlp model
+
     vocab = list(nlp.vocab.strings)     
     vocab_size = len(vocab)              
     embed_dim = nlp(vocab[0]).vector.shape[0]
-    vectors = np.zeros((vocab_size, embed_dim)) 
+    
+    # Initialize the three containers
+    vectors = [np.zeros(embed_dim)] 
     key2row = {}    
     words = []      
-    for index, word in enumerate(vocab):
-        vector = nlp(word).vector
-        key = hash_string(word)
-        key2row[key] =  index   # key and row of corresponding vector
-        vectors[index] =  vector   
-        words.append(word)
 
-    save_file(vectors, filename='vectors')
-    save_file(key2row, filename='key2row')
-    save_file(words, filename='words')
+    # Iterate on vocabulary and append in containers
+    index = 1
+    for word in vocab:
+        words.append(word)
+        vector = nlp(word).vector
+        if vector.any() != np.zeros(embed_dim).any():   # Avoid words with embedding vectors as zero vector
+            key = hash_string(word)
+            key2row[key] =  index   # key and row of corresponding vector
+            vectors.append(vector)
+            index += 1      # increment
+        
+    # Save files
+    save_file(vectors, os.path.join(model, 'vectors'))
+    save_file(key2row, os.path.join(model, 'key2row'))
+    save_file(words, os.path.join(model, 'words'))
     
 
 if __name__ == "__main__":
-    nlp = spacy.load("en_core_web_sm")
-    create_lang_model(nlp)
+    # Note: Install the required spacy model by
+    # python -m spacy download model_name
+    model = "en_core_web_sm"
+    create_lang_model(model)
 
